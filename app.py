01051fbb4d0de27bd7b79d353742d371551cb06e
Matija Basanovic <mbasanov@gmail.com>
2019-12-01 17:13:07 +0000

    Using enviroment variables

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ import os
 from flask import Flask
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from dotenv import load_dotenv
+load_dotenv()
+
+USERNAME = os.getenv('MONGODB_USERNAME')
+PASSWORD = os.getenv('MONGODB_PASSWORD')
+COLLECTION_NAME = os.getenv('MONGODB_COLLECTION_NAME')
 
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'food_factory'
-app.config["MONGO_URI"] = ''
+app.config["MONGO_URI"] = 'mongodb+srv://{USERNAME}:{PASSWORD}@myfirstcluster-y6kfn.mongodb.net/{COLLECTION_NAME}?retryWrites=true&w=majority'
+print(COLLECTION_NAME)
 
 mongo = PyMongo(app)
 
