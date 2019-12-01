668db6e764f4edc5035343011731b6f439f522ce
Matija Basanovic <mbasanov@gmail.com>
2019-12-01 21:31:47 +0000

    Reading data from MongoDB and showing in the app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
@@ -13,15 +13,19 @@ COLLECTION_NAME = os.getenv('MONGODB_COLLECTION_NAME')
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'food_factory'
-app.config["MONGO_URI"] = 'mongodb+srv://{USERNAME}:{PASSWORD}@myfirstcluster-y6kfn.mongodb.net/{COLLECTION_NAME}?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://' + USERNAME + ':' + PASSWORD + \
+    '@myfirstcluster-y6kfn.mongodb.net/' + \
+    COLLECTION_NAME + '?retryWrites=true&w=majority'
+
 print(COLLECTION_NAME)
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello..World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
