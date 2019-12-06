28cf87bffce6c64123486ddae7ec0866d0d835fa
Matija Basanovic <mbasanov@gmail.com>
2019-12-06 11:59:18 +0000

    Finish Add and Remove line for ingredients in JS

--- static/js/script.js
+++ static/js/script.js
@@ -3,23 +3,41 @@
 * For add_recipe.html & edit_recipe.html pages
 */
 
-let ingCount = $(".ingredients").length;
+let ingredientsCount = $(".ingredients").length;
 
 /* Add new line */
-$(".add-ing").click(function() {
-    /* Clone line, insert before add/remove btns and clear existing values */
+$(".add-ing").click(function () {
     $(".ingredients:first").clone()
         .insertBefore(".add-ing")
         .find("input[type='text']").val("");
-    /* Ensures original line is never removed */
-    ingCount += 1;
+    ingredientsCount += 1;
 });
 
 /* Remove last line */
-$(".remove-ing").click(function() {
-    /* Ensure that the first line can't be removed */
-    if (ingCount > 1) {
+$(".remove-ing").click(function () {
+    if (ingredientsCount > 1) {
         $(".ingredients:last").remove();
-        ingCount -= 1;
+        ingredientsCount -= 1;
+    }
+});
+
+/*
+* Add and remove new instruction lines on user click
+*/
+let instructionCount = $(".instructions").length;
+
+/* Add new line */
+$(".add-instruction").click(function () {
+    $(".instructions:first").clone()
+        .insertBefore(".add-instruction")
+        .find("input[type='text']").val("");
+    instructionCount += 1;
+});
+
+/* Remove last line */
+$(".remove-instruction").click(function () {
+    if (instructionCount > 1) {
+        $(".instructions:last").remove();
+        instructionCount -= 1;
     }
 });
