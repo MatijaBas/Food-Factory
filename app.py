9a98361d6112a08e418d58fd39c576d193ec83f4
Matija Basanovic <mbasanov@gmail.com>
2019-12-14 23:32:47 +0000

    Changing port to pass application error on heroku

--- app.py
+++ app.py
@@ -146,7 +146,9 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+port = int(os.environ.get('PORT', 8080))
+
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', '8080')),
+    app.run(host='0.0.0.0',
+            port=port,
             debug=True)
